b30ccc65ff2435b0eec3fcde7325d421f6abc3c2
Hamza AIT BEN YISSA <hamzaaitbenyissa@gmail.com>
2022-04-10 16:00:38 +0000

    answer class

---
+++ lib/answer.dart
@@ -0,0 +1,24 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final VoidCallback pressHandler;
+  String AnswerText;
+  Answer(this.pressHandler,this.AnswerText);
+
+  // difference between const and final
+  // final will be const once the first value is affected
+  //ther is also a difference between const name= var [] and var name= const []
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        margin: EdgeInsets.all(6),
+        child: RaisedButton(
+            color: Colors.blue,
+            child: Text(AnswerText),
+            onPressed: pressHandler));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app1/answer.dart';
 import 'package:app1/question.dart';
 import 'package:flutter/material.dart';
 
@@ -15,19 +16,38 @@ class MyApp extends StatefulWidget {
 
 class MyAppstate extends State<MyApp> {
   var questionindex = 0;
-  void answerQuestion() {
+
+  List<Map<String, dynamic>> questions = [
+    {
+      "question": "What is your favorite color ?",
+      "answers": ["Red", "Green", "Yellow"]
+    },
+    {
+      "question": "What is your favorite animal ?",
+      "answers": ["Cat", "Dog", "Rabbit"]
+    },
+    {
+      "question": "What is your favorite hobbie ?",
+      "answers": ["Football", "Guitar", "Swimming"]
+    },
+  ];
+
+  var _results = {
+    'Key_1': "Value_1",
+    'Key_2': "Value_2",
+  };
+
+  void _answerQuestion() {
+
+    if(questionindex < questions.length-1){
     setState(() {
       questionindex = questionindex + 1;
     });
     print(questionindex);
+    }
+
   }
 
-  var questions = [
-    "What is your favorite color ?",
-    "What is your favorite animal ?",
-    "What is your favorite movie ?",
-    "What is your favorite hobbie ?"
-  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -37,10 +57,10 @@ class MyAppstate extends State<MyApp> {
         backgroundColor: const Color.fromRGBO(11, 0, 98, 0.8),
       ),
       body: Column(children: [
-        Question(questions[questionindex]),
-        RaisedButton(child: const Text("Answer 1"), onPressed: answerQuestion),
-        RaisedButton(child: const Text("Answer 2"), onPressed: answerQuestion),
-        RaisedButton(child: const Text("Answer 3"), onPressed: answerQuestion),
+        Question(questions[questionindex]['question']),
+        // ... means we get tha values of list because map return list
+        ...(questions[questionindex]['answers'])
+            .map((answer) => Answer(_answerQuestion, answer))
       ]),
       backgroundColor: Color.fromRGBO(255, 255, 255, 0.8),
     ));
