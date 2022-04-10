4b717e2e77ac4c51f15b50e678caf9cf4554b56f
Hamza AIT BEN YISSA <hamzaaitbenyissa@gmail.com>
2022-04-10 12:37:51 +0000

    add questions design

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,47 @@
+import 'package:app1/question.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppstate();
+  }
+}
 
+class MyAppstate extends State<MyApp> {
+  var questionindex = 0;
+  void answerQuestion() {
+    setState(() {
+      questionindex = questionindex + 1;
+    });
+    print(questionindex);
+  }
+
+  var questions = [
+    "What is your favorite color ?",
+    "What is your favorite animal ?",
+    "What is your favorite movie ?",
+    "What is your favorite hobbie ?"
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        title: Text("Quiz App"),
-        backgroundColor: Color.fromRGBO(11, 0, 98,0.8),
+        title: const Text("Quiz App"),
+        backgroundColor: const Color.fromRGBO(11, 0, 98, 0.8),
       ),
-      body: Text("This is a quiz app"),
+      body: Column(children: [
+        Question(questions[questionindex]),
+        RaisedButton(child: const Text("Answer 1"), onPressed: answerQuestion),
+        RaisedButton(child: const Text("Answer 2"), onPressed: answerQuestion),
+        RaisedButton(child: const Text("Answer 3"), onPressed: answerQuestion),
+      ]),
       backgroundColor: Color.fromRGBO(255, 255, 255, 0.8),
     ));
   }
---
+++ lib/question.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  String questionText;
+  Question(this.questionText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.all(10),
+      child: Text(questionText,
+          style: const TextStyle(fontSize: 24), textAlign: TextAlign.center),
+    );
+  }
+}
